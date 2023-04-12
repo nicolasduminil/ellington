447a4f6c4979fb4c28d7142a4c16834eedb939c1
Nicolas DUMINIL <nicolas.duminil@simplex-software.fr>
2023-04-12 23:11:41 +0200

    ECS task definition

--- deploy.sh
+++ deploy.sh
@@ -5,4 +5,5 @@ aws cloudformation wait stack-create-complete --stack-name duke-stack
 ECR_URI=$(sam list stack-outputs --stack-name duke-stack --output json | jq -r ".[0].OutputValue")
 aws ecr get-login-password --region $AWS_REGION | docker login --username AWS --password-stdin $ECR_URI
 mvn -Paws -DECR_URI=$ECR_URI -DskipITs clean verify
+aws ecs create-cluster --cluster-name duke-cluster
 
